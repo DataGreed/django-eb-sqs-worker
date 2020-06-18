b1a24535477cca65b13a71428067e24b5ace5542
Alexey Strelkov <cpr.al.exe@gmail.com>
2020-06-18 04:14:33 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-eb-sqs-worker", # Replace with your own username
+    version="0.1.0",
+    author="Alexey Strelkov",
+    author_email="datagreed@gmail.com",
+    description="Django Background Tasks for Amazon Elastic Beanstalk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DataGreed/django-eb-sqs-worker/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
